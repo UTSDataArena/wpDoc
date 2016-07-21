bb3eae7338556829684e50ca5f23de172a8d88b4
davm <utsdavm@gmail.com>
2016-07-21 05:58:34 +0000

    first commit

---
+++ wp-content/plugins/2j-slideshow/inc/setup.php
@@ -0,0 +1,15 @@
+<?php
+/*  
+ * 2J SlideShow		http://2joomla.net/wordpress
+ * Version:           1.2.17 - 21851
+ * Author:            2J Team (c)
+ * Author URI:        http://2joomla.net/wordpress
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Date:              Fri, 08 Jul 2016 07:50:53 GMT
+ */
+
+class TwojSlideshowActivator {
+	public static function activate() {}
+	public static function deactivate() {}
+}
